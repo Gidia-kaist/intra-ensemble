855158fed6f2dc4ee840578e2015c20eb573c6b0
Woon Hyung Cheong <cheongwh@kaist.ac.kr>
2021-01-06 20:43:06 +0900

    Ensemble_확실_ver운형

--- ensemble_MLP.py
+++ ensemble_MLP.py
@@ -1,42 +1,41 @@
-#from torchvision import datasets
+# from torchvision import datasets
 from torch.utils.data import DataLoader, TensorDataset
-#import torchvision.transforms as transforms
+# import torchvision.transforms as transforms
 import torch
 import random
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import wandb
+import copy
 
 # CUDA 처리
 USE_CUDA = torch.cuda.is_available()
-device = torch.device("cuda" if USE_CUDA else "cpu") # GPU 사용 가능하면 사용하고 아니면 CPU 사용
+device = torch.device("cuda" if USE_CUDA else "cpu")  # GPU 사용 가능하면 사용하고 아니면 CPU 사용
 print("다음 기기로 학습합니다:", device)
 
 # COMMAND
 hidden_size = 512
 switch_ensemble = 0
-probability = 0.3
-learning_rate = 0.1
+probability = 0.7
+learning_rate = 0.05
 batch_size = 128
 # number of epochs to train the model
-n_epochs = 1000   # suggest training between 20-50 epochs
-wandb.init(project="intra-ensemble", entity='fust', config = {"hidden_size": hidden_size,
-                                                              "switch_ensemble": switch_ensemble,
-                                                              "probability": probability,
-                                                              "learning_rate": learning_rate,
-                                                              "batch_size": batch_size,
-                                                              "n_epochs": n_epochs})
+n_epochs = 5  # suggest training between 20-50 epochs
+wandb.init(project="intra-ensemble", entity='fust', config={"hidden_size": hidden_size,
+                                                            "switch_ensemble": switch_ensemble,
+                                                            "probability": probability,
+                                                            "learning_rate": learning_rate,
+                                                            "batch_size": batch_size,
+                                                            "n_epochs": n_epochs})
 
 hidden_size = wandb.config.hidden_size
 switch_ensemble = wandb.config.switch_ensemble
-probability = 1-wandb.config.probability
+probability = wandb.config.probability
+
 learning_rate = wandb.config.learning_rate
 n_epochs = wandb.config.n_epochs
 batch_size = wandb.config.batch_size
-mask2 = torch.bernoulli(probability*torch.ones(hidden_size, 784)).to(device)
-print(mask2.sum()/(hidden_size*784))
-
 
 if switch_ensemble == 1:
     switch_ensemble = True
@@ -48,7 +47,6 @@ random.seed(777)
 torch.manual_seed(777)
 if device == 'cuda':
     torch.cuda.manual_seed_all(777)
-
 
 MNIST_data = np.load("/home/kdh/MNIST_data/MNIST_treated.npz")
 MNIST_data.files
@@ -76,14 +74,13 @@ test_loader = DataLoader(test, batch_size=10000)
 train_loader = DataLoader(train, batch_size=batch_size)
 
 
-
 ## Define the NN architecture
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(28 * 28, hidden_size)
-
-        self.fc2 = nn.Linear(hidden_size, 10)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, 10)
         # linear layer (n_hidden -> 10)
         # dropout layer (p=0.2)
         # dropout prevents overfitting of data
@@ -94,16 +91,15 @@ class Net(nn.Module):
         x = x.view(-1, 28 * 28)
         # add hidden layer, with relu activation function
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
-
 
 
 # initialize the NN
 model = Net()
 model.cuda()
 print(model)
-
 
 ## Specify loss and optimization functions
 
@@ -113,18 +109,18 @@ criterion = nn.CrossEntropyLoss()
 # specify optimizer
 optimizer = torch.optim.SGD(model.parameters(), momentum=0.9, lr=learning_rate)
 
-
 epoch_counter = 0
-#model.train()  # prep model for training
+# model.train()  # prep model for training
 
 
 print('ensemble? :', switch_ensemble)
-print('probability? :', 1-probability)
+print('probability? :', probability)
 for epoch in range(n_epochs):
     # monitor training loss
     torch.cuda.empty_cache()
     train_loss = 0.0
     test_loss = 0.0
+    count = 0
     class_correct = list(0. for i in range(10))
     class_total = list(0. for i in range(10))
     ###################
@@ -134,28 +130,34 @@ for epoch in range(n_epochs):
     for data, target in train_loader:
 
         if switch_ensemble is True:
-            # print("Ensemble:", switch_ensemble)
-            weight_fc1 = model.fc1.weight
-            weight_fc2 = model.fc2.weight
-#            print(weight)
-#            print(F.dropout(weight, p=probability)*(1-probability))
-            model.fc1.weight = torch.nn.Parameter(F.dropout(weight_fc1, p=probability)*(1-probability))
-            model.fc2.weight = torch.nn.Parameter(F.dropout(weight_fc2, p=probability)*(1-probability))
-        # linear layer (n_hidden -> hidden_2)
-        # clear the gradients of all optimized variables
+            with torch.no_grad():
+                mask1 = torch.bernoulli(probability * torch.ones(hidden_size, 784)).to(device)
+                mask2 = torch.bernoulli(probability * torch.ones(hidden_size, hidden_size)).to(device)
+                a = torch.mul(mask1, model.fc1.weight)
+                b = torch.mul(mask2, model.fc2.weight)
+                c = torch.mul(1 - mask1, model.fc1.weight)
+                d = torch.mul(1 - mask2, model.fc2.weight)
+                model.fc1.weight.data = a
+                model.fc2.weight.data = b
+
         optimizer.zero_grad()
-        # forward pass: compute predicted outputs by passing inputs to the model
         output = model(data)
-        if switch_ensemble is True:
-            model.fc1.weight = torch.nn.Parameter(weight_fc1)
-            model.fc2.weight = torch.nn.Parameter(weight_fc2)
+
+
         # calculate the loss
         loss = criterion(output, target)
         # backward pass: compute gradient of the loss with respect to model parameters
         loss.backward()
         # perform a single optimization step (parameter update)
-        optimizer.step()
+
+        # print("#1", (model.fc1.weight.sum()))
+        optimizer.step() # 마스크 숭숭
+        # print("#2", (model.fc1.weight.sum()))
         # update running training loss
+        if switch_ensemble is True:
+            model.fc1.weight.data = model.fc1.weight.data + c
+            model.fc2.weight.data = model.fc2.weight.data + d
+
         train_loss += loss.item() * data.size(0)
 
     for data, target in test_loader:
@@ -193,7 +195,7 @@ test_loss = 0.0
 class_correct = list(0. for i in range(10))
 class_total = list(0. for i in range(10))
 
-#model.eval() # prep model for *evaluation*
+# model.eval() # prep model for *evaluation*
 
 for data, target in test_loader:
     torch.cuda.empty_cache()
@@ -202,7 +204,7 @@ for data, target in test_loader:
     # calculate the loss
     loss = criterion(output, target)
     # update test loss
-    test_loss += loss.item()*data.size(0)
+    test_loss += loss.item() * data.size(0)
     # convert output probabilities to predicted class
     _, pred = torch.max(output, 1)
     # compare predictions to true label
@@ -214,7 +216,7 @@ for data, target in test_loader:
         class_total[label] += 1
 
 # calculate and print avg test loss
-test_loss = test_loss/len(test_loader.dataset)
+test_loss = test_loss / len(test_loader.dataset)
 wandb.log({"test_loss": test_loss})
 print('Test Loss: {:.6f}\n'.format(test_loss))
 
